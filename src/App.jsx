283b8942074f9c735b494bd9e6d2ba0e89ff3eba
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-07-08 19:42:54 +0530

    Fragment: alternate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from "react";
+import { useState } from "react";
 import componentsImage from "./assets/components.png";
 import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import { CoreConcepts } from "./components/CoreConcepts.jsx";
@@ -27,7 +27,7 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -69,7 +69,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </Fragment>
+    </>
   );
 }
 
