6a89aa0bafb6302c5ab05891d6736b9d8edba347
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-07-08 14:14:41 +0530

    outputting dynamic data - using key in props

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ function App() {
         <section id="core-concepts">
           <h2>Core concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map((core_concepts) => (
-              <CoreConcepts {...core_concepts} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcepts key={conceptItem.title} {...conceptItem} />
             ))}
           </ul>
         </section>
