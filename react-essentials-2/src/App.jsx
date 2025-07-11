52d42513ce86c16fc4a63e430ae91a2f3d4f1e4d
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-07-11 20:33:31 +0530

    react-tictactoe: add edit button for players

--- react-essentials-2/src/App.jsx
+++ react-essentials-2/src/App.jsx
@@ -8,12 +8,14 @@ function App() {
               <span className="player-name">Player 1</span>
               <span className="player-symbol">X</span>
             </span>
+            <button>Edit</button>
           </li>
           <li>
             <span className="player">
               <span className="player-name">Player 2</span>
               <span className="player-symbol">O</span>
             </span>
+            <button>Edit</button>
           </li>
         </ol>
       </div>
