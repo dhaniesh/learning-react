48d973f3331c19cdedc386c3f40ee2e4b552aa34
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-07-11 21:32:42 +0530

    react-tictactoe: add player edit state

--- react-essentials-2/src/components/Player.jsx
+++ react-essentials-2/src/components/Player.jsx
@@ -1,11 +1,25 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((isEditing)=>!isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  //   let btnContent = "Save";
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name}/>;
+    // btnContent = "Edit"
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
