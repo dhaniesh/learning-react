a680edb4e90fbfd9002d77988ca50a8c99900d8f
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-07-12 13:24:58 +0530

    react-tictactoe: add player name edit functionality

--- react-essentials-2/src/components/Player.jsx
+++ react-essentials-2/src/components/Player.jsx
@@ -2,21 +2,25 @@ import { useState } from "react";
 
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, SetPlayerName] = useState(name);
   function handleEditClick() {
-    setIsEditing((isEditing)=>!isEditing);
+    setIsEditing((isEditing) => !isEditing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
-  //   let btnContent = "Save";
+  function handleChange(event) {
+    SetPlayerName(event.target.value);
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name}/>;
-    // btnContent = "Edit"
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
