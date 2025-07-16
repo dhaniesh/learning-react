267cbc2bd59dbebc695e1f3a1f5b25c516e04441
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-07-16 15:32:59 +0530

    react-tictactoe: add gameboard component

--- react-essentials-2/src/App.jsx
+++ react-essentials-2/src/App.jsx
@@ -1,5 +1,6 @@
+import GameBoard from "./components/GameBoard.jsx";
 import Player from "./components/Player.jsx";
-
+GameBoard
 function App() {
   return (
     <main>
@@ -8,6 +9,7 @@ function App() {
           <Player name="Player 1" symbol="X" />
           <Player name="Player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
---
+++ react-essentials-2/src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+const initialGameBoard = [
+  [null, "x", null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
