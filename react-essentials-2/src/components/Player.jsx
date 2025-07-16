0459794f82ee62a82b54a278d40e3613bcb621c6
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-07-16 16:56:00 +0530

    react-tictactoe: set active player to child components by using lifting the state

--- react-essentials-2/src/components/Player.jsx
+++ react-essentials-2/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ name, symbol, isActive }) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, SetPlayerName] = useState(name);
   function handleEditClick() {
@@ -18,7 +18,7 @@ export default function Player({ name, symbol }) {
     );
   }
   return (
-    <li>
+    <li className={isActive ? "active": undefined} >
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
