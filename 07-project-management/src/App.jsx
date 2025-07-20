be28b55f6cd9446bde505be28d80015c9a3ce7b9
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-07-20 20:57:59 +0530

    project-management: Implement project deletion functionality

--- 07-project-management/src/App.jsx
+++ 07-project-management/src/App.jsx
@@ -10,6 +10,18 @@ function App() {
     projects: [],
   });
 
+  function handleDelete() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
   function handleStartAddProject() {
     setProjectsState((prevState) => {
       return {
@@ -55,7 +67,7 @@ function App() {
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId
   );
-  let content = <SelectedProject project={selectedProject} />;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDelete}/>;
 
   if (projectsState.selectedProjectId === null) {
     content = (
