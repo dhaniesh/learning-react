980176a261144e16b89314f3e8b71c30e847b52c
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-07-27 00:15:09 +0530

    side-effects: Add progress bar to DeleteConfirmation

--- 09-side-effects/src/components/DeleteConfirmation.jsx
+++ 09-side-effects/src/components/DeleteConfirmation.jsx
@@ -1,6 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
+const TIMER = 3000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+
+  const [remainingTime, setRemainingTime] = useState(TIMER)
+
+  useEffect(()=>{
+    const interval = setInterval(() => {
+    console.log('INTREVAL')
+    setRemainingTime( (prevTime)=> prevTime - 20 )
+  }, 20);
+
+  return ()=> {
+    clearInterval(interval);
+  }
+  }, [])
+
   useEffect(() => {
     console.log('TIMER SET')
     const timer = setTimeout(()=>{
@@ -26,6 +41,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
