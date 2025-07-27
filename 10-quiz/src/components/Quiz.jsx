b68122318813dd6bca41ff623a502eb8a8e15232
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-07-27 16:25:26 +0530

    quiz-app: Add quiz completion view and shuffle answers

--- 10-quiz/src/components/Quiz.jsx
+++ 10-quiz/src/components/Quiz.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
-
+import quizCompleteImg from '../assets/quiz-complete.png'
 function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+
+  const quizComplete = activeQuestionIndex === QUESTIONS.length;
+  if (quizComplete) {
+    return (
+        <div id="summary">
+<img src={quizCompleteImg} alt="Trophy image" /><h2>Quiz completed</h2>
+        </div>
+    )
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers].sort(
+    () => Math.random() - 0.5
+  );
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
-  const activeQuestionIndex = userAnswers.length;
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
