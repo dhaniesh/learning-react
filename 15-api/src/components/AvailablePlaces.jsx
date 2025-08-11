9550946198aabe602221aa36b2971fb4acfa080d
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-08-11 16:56:17 +0530

    Initialize project with core files and directory structure

---
+++ 15-api/src/components/AvailablePlaces.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Places from "./Places.jsx";
+import { useEffect } from "react";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        setAvailablePlaces(result.places);
+      });
+  }, []);
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
