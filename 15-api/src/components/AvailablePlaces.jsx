b5289260d3c37c0d0f427ab9880ee04457914873
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-08-11 17:17:56 +0530

    Handle fetch errors and display them in AvailablePlaces
    - Introduce `Error` component for consistent error message display.
    - Implement `try...catch` block in `AvailablePlaces` to catch and manage API fetch errors.
    - Display a user-friendly error message when places cannot be fetched.
    - Update places API endpoint from `/places` to `/place`.

--- 15-api/src/components/AvailablePlaces.jsx
+++ 15-api/src/components/AvailablePlaces.jsx
@@ -1,20 +1,37 @@
 import { useState } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 import { useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const result = await response.json();
-      setAvailablePlaces(result.places);
+
+      try {
+        const response = await fetch("http://localhost:3000/place");
+        const result = await response.json();
+        if (!response.ok) {
+          throw new Error("failed to fetch places");
+        }
+        setAvailablePlaces(result.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later",
+        });
+      }
       setIsFetching(false);
     }
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occured" message={error.message} />;
+  }
   return (
     <Places
       title="Available Places"
