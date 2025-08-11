02847f2dde5754182807f1635d8d420b8e7a92e3
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-08-11 17:06:29 +0530

    feat: Implement loading state for place data fetching
    Introduce `isFetching` state in `AvailablePlaces` to track the API call status. Pass `isLoading` and `loadingText` props to the `Places` component, which now displays a loading message while place data is being fetched. This improves user feedback during data retrieval.

--- 15-api/src/components/AvailablePlaces.jsx
+++ 15-api/src/components/AvailablePlaces.jsx
@@ -3,12 +3,15 @@ import Places from "./Places.jsx";
 import { useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   useEffect(() => {
     async function fetchPlaces() {
-      const resonse = await fetch("http://localhost:3000/places");
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
       const result = await response.json();
       setAvailablePlaces(result.places);
+      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
@@ -16,6 +19,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching places data."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
