1cb7edaca2464678100f454f68623146ee67a7f8
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-08-25 16:19:15 +0530

    feat: Add API call to persist user selected places
    - Implement `updateUserSelectedPlaces` in `http.js` to send user selections via PUT request.
    - Integrate `updateUserSelectedPlaces` into `handleSelectPlace` to save newly selected places to the backend.

--- 15-api/src/http.js
+++ 15-api/src/http.js
@@ -6,3 +6,18 @@ export async function fetchAvailablePlaces() {
     }
     return result.places;
 }
+
+export async function updateUserSelectedPlaces(places) {
+    const response = await fetch("http://localhost:3000/user-places", {
+        method: "PUT",
+        body: JSON.stringify({ places }),
+        headers: {
+            'Content-Type': "application/json"
+        }
+    });
+    const result = await response.json();
+    if (!response.ok) {
+        throw new Error("failed to update user selected place")
+    }
+    return response.message;
+}
